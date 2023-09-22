2e4f46a8b54d7549f0cc32fbe0b2db3ad6ed43e0
reshmi-02 <reshmihash123@gmail.com>
2023-09-22 14:26:55 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+var input=[10,30,50];
+document.write("given array=[ "+input+" ]<br><br>");
+
+let res=input.map(function(a){
+  if(true){
+    var b=(3.14*a*a);
+  }
+  return b;
+})
+document.write("answer= [ "+res+" ]");
